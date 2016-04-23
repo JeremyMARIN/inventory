f35caa399720fef2147c0df07acf682cc4605bed
Jeremy MARIN <jgm438@nyu.edu>
2016-04-22 21:56:31 -0400

    Part B of the assignment done

--- inventory.php
+++ inventory.php
@@ -8,14 +8,65 @@ function getList() {
 
 	while (!feof($fp)) {
 		$line = fgetcsv($fp, 1024);
-		$list[] = array("title" => $line[0], "file" => $line[1], "stock" => intval($line[2]), "price" => $line[3] );
+		$list[] = array("id" => $line[0], "title" => $line[1], "file" => $line[2], "stock" => intval($line[3]), "price" => $line[4] );
 	}
 
-	fclose($fp) or die("Can't close file 'inventory_db.csv'...");
+	fclose($fp) or die("Can't close file 'inventory_inventory.csv'...");
 
 	return $list;
 }
 
-echo json_encode(getList());
+function getUpdates($form) {
+	$updates = array();
+
+	foreach ($form as $key => $value) { // build an array of updates
+		$temp = explode("_", $key);
+		$id = $temp[0];
+		$type = $temp[1];
+		if ($type == "stock")
+			$updates[$id][$type] = intval($value);
+		else
+			$updates[$id][$type] = floatval($value);
+	}
+
+	return $updates;
+}
+
+function updateInventory($updates) {
+	$inventory = array();
+
+	// open the file for reading
+	$fp = fopen("inventory_db.csv", "r") or die("Can't open file 'inventory_db.csv' for reading...");
+
+	while (!feof($fp)) {
+		$line = fgetcsv($fp, 1024);
+		$id = $line[0];
+		if ($id != null) { // check if the line is not an empty line
+			$line[3] = $updates[$id]["stock"];
+			$line[4] = $updates[$id]["price"];
+			$inventory[] = $line;
+		}
+	}
+
+	fclose($fp) or die("Can't close file 'inventory_db.csv'...");
+
+
+	// open the file for writing
+	$fp = fopen("inventory_db.csv", "w") or die("Can't open file 'inventory_db.csv' for writing...");
+
+	foreach ($inventory as $value) {
+		fputcsv($fp, $value); // write the updated value
+	}
+
+	fclose($fp) or die("Can't close file 'inventory_db.csv'...");
+}
+
+
+if (count($_POST) > 0) { // if the request's method is POST
+	updateInventory(getUpdates($_POST));
+	header("Location: admin.html"); // redirect the user
+} else {
+	echo json_encode(getList());
+}
 
 ?>
--- js/admin.js
+++ js/admin.js
@@ -1,130 +1,60 @@
 function init() {
-	var dataContent            = document.getElementById("data");
-	var svgContent             = document.getElementById("svg");
-	var displayListButton      = document.getElementById("display_list");
-	var displayHistogramButton = document.getElementById("display_histogram");
+	var listContent = document.getElementById("list");
 
-	displayListButton.addEventListener("click", function() {
-		displayList(dataContent, svgContent);
-	});
-
-	displayHistogramButton.addEventListener("click", function() {
-		displayHistogram(dataContent, svgContent);
-	});
-
-	displayList(dataContent);
+	displayList(listContent);
 }
 
-
-function displayList(dataContent, svgContent) {
+function displayList(listContent) {
 	var xhr = new XMLHttpRequest();
-	var params = "display=list";
 
-	xhr.open("POST", "admin.php");
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.open("GET", "inventory.php");
 
 	xhr.addEventListener("readystatechange", function() {
 		if (xhr.readyState == 4 && xhr.status == 200) {
-			processListData(xhr.responseText, dataContent, svgContent);
+			processListData(xhr.responseText, listContent);
 		} else if (xhr.readyState == 4) {
-			dataContent.innerHTML = "Error " + xhr.status + ".";
+			listContent.innerHTML = "Error " + xhr.status + ".";
 		}
 	}, false);
 
-	xhr.send(params);
+	xhr.send();
 }
-function processListData(data, dataContent, svgContent) {
+
+function processListData(data, listContent) {
 	var formatedData = ""; // HTML formated data
 
 	data = JSON.parse(data);
 
 	for (var i = 0, length = data.length; i < length; i++) {
-		if (data[i].email && data[i].choice) {
-			var line = "<tr><td>" + data[i].email + "</td><td>" + data[i].choice + "</td></tr>";
+		console.log(data[i]);
+		if (data[i].title != null && data[i].file != null && data[i].stock != null && data[i].price != null) {
+			var line = "";
+			if (i % 4 == 0) // every 4 games, start a new row
+				line += "<div class=\"row\">";
+
+			line += "<div class=\"game\">";
+			line += "<div class=\"game-container radius\"><div>";
+			if (data[i].stock == 0)
+				line += "<div class=\"picture sold-out\"></div>";
+			line += "<div class=\"picture ";
+			if (data[i].stock == 0)
+				line += "gray";
+			line += "\" style=\"background-image: url('img/" + data[i].file + "');\"></div>";
+			line += "</div><div class=\"information radius\">";
+			line += "<h4 class=\"title\">" + data[i].title + "</h4>";
+
+			line += "<div><label for=\"stock\">Stock: </label><input id=\"stock\" type=\"text\" name=\"" + data[i].id + "_stock\" value=\"" + data[i].stock + "\" size=\"3\" class=\"radius text-centered\" /></div>";
+			line += "<div style=\"margin-top: 4px;\"><label for=\"price\">Price ($): </label><input id=\"price\" type=\"text\" name=\"" + data[i].id + "_price\" value=\"" + data[i].price + "\" size=\"3\" class=\"radius text-centered\" /></div>";
+			line += "</div></div></div>";
+
+			if (i % 4 == 3) // close the row
+				line += "</div>";
+
 			formatedData += line;
+		} else {
+			console.log("Problem with: " + data[i].title);
 		}
 	}
 
-	dataContent.innerHTML = 
-		"<table>" +
-			"<tr>" + 
-				"<th>Email</th>" +
-				"<th>Choice</th>" +
-			"</tr>" +
-			formatedData +
-		"</table>"
-	;
-
-	if (svgContent)
-		svgContent.style.display = "none";
-	dataContent.style.display = "block";
-}
-
-
-function displayHistogram(dataContent, svgContent) {
-	var xhr = new XMLHttpRequest();
-	var params = "display=histogram";
-
-	xhr.open("POST", "admin.php");
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-	xhr.addEventListener("readystatechange", function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			processHistogramData(xhr.responseText, dataContent, svgContent);
-		} else if (xhr.readyState == 4) {
-			dataContent.innerHTML = "Error " + xhr.status + ".";
-		}
-	}, false);
-
-	xhr.send(params);
-}
-function processHistogramData(data, dataContent, svgContent) {
-	var width = 500, height = 200; // size of the SVG canvas
-	var colors = ["red", "blue", "green", "yellow", "orange"];
-
-	data = JSON.parse(data);
-	console.log(data);
-
-	var percentages = [data.percentages.a, data.percentages.b, data.percentages.c, data.percentages.d, data.percentages.e];
-	var counts      = [data.counts.a, data.counts.b, data.counts.c, data.counts.d, data.counts.e];
-	var titles      = ["a", "b", "c", "d", "e"];
-
-	for (var i = 0, max = counts.length; i < max; i++) {
-		var percentage = percentages[i] ? percentages[i] : 0;
-		var count      = counts[i] ? counts[i] : 0;
-		var title      = titles[i] ? titles[i] : 0;
-
-		var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-		rect.setAttribute("x", (width / 5) * i);
-		rect.setAttribute("y", (1 - percentage) * height);
-		rect.setAttribute("width", (width / 5) * 0.8);
-		rect.setAttribute("height", percentage * height);
-		rect.setAttribute("fill", colors[i]);
-		svgContent.appendChild(rect);
-
-		var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-		text.setAttribute("x", ( (width / 5) * i) + (width / 15) );
-		text.setAttribute("y", ((1 - percentage) * height) - (height / 20) );
-		text.setAttribute("font-size", height/10);
-		text.setAttribute("fill", colors[i]);
-
-		var textNode = document.createTextNode(count);
-		text.appendChild(textNode);
-		svgContent.appendChild(text);
-
-		text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-		text.setAttribute("x", ( (width / 5) * i) + (width / 15) );
-		text.setAttribute("y", height + 20);
-		text.setAttribute("font-size", height/10);
-		text.setAttribute("fill", "white");
-		
-		textNode = document.createTextNode(title);
-		text.appendChild(textNode);
-		svgContent.appendChild(text);
-	}
-
-	dataContent.style.display = "none";
-	svgContent.style.width = width;
-	svgContent.style.height = height + 20;
-	svgContent.style.display = "block";
+	listContent.innerHTML = formatedData;
 }
